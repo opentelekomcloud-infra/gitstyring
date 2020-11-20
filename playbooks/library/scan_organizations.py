55430dd3ba386fbe259e94a245d06b2259fdab04
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-11-20 12:47:30 +0300

    added module for scanning folder structure

---
+++ playbooks/library/scan_organizations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import os
+
+import yaml
+from ansible.module_utils.basic import *
+
+
+def main():
+    module = AnsibleModule(argument_spec={})
+    output = {}
+    for root, dirs, files in os.walk("../orgs"):
+        for file in files:
+            current_root = os.path.basename(root)
+            a_yaml_file = open(os.path.join(root, file))
+            parsed_yaml_file = yaml.load(a_yaml_file)
+            parent = os.path.basename(os.path.abspath(os.path.join(root, os.pardir)))
+            if parent in output:
+                if current_root in output[parent]:
+                    output[parent][current_root].update(parsed_yaml_file)
+                else:
+                    output[parent].update({current_root: parsed_yaml_file})
+            else:
+                output.update({parent: {current_root: parsed_yaml_file}})
+    module.exit_json(changed=True, data=output)
+
+
+if __name__ == '__main__':
+    main()
