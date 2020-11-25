e76fe21b92f03c786c9194f6d43b9d2773b2d72c
Anton Sidelnikov <53078276+anton-sidelnikov@users.noreply.github.com>
2020-11-25 14:43:20 +0300

    Management repo / basic and collaborators settings (#1)
    * added options
    
    * added checks repos in organizations
    
    * fix endline
    
    * update base
    
    * added module arg
    
    * collaborator managing first iter
    
    * collaborator managing fixed
    
    * branch protection dummy
    
    * minor fixes

---
+++ roles/gitcontrol/files/github_api.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+import os
+from argparse import ArgumentParser
+
+import requests as requests
+import yaml
+
+github_user = os.getenv('GITHUB_USER')
+github_token = os.getenv('GITHUB_TOKEN')
+headers = {'Authorization': f'token {github_token}'}
+bad_statuses = [400, 404]
+
+
+def read_yaml_file(path, org, endpoint, repo_name=None):
+    if endpoint == 'manage_collaborators':
+        path += f'/{org}/repositories/{repo_name}.yml'
+    with open(path, 'r') as file:
+        data = yaml.load(file)
+    return data
+
+
+def manage_collaborators(github_api, owner, repo_name, repo):
+    output = ''
+    collaborators = repo[repo_name]['collaborators']
+    for clb in collaborators:
+        if collaborators[clb]:
+            for user in collaborators[clb]:
+                res = requests.put(
+                    f'{github_api}/repos/{owner}/{repo_name}/collaborators/{user}',
+                    json={'permission': clb},
+                    headers=headers,
+                    timeout=15)
+                if res.status_code in bad_statuses:
+                    output += f'user {user} not created: {res.status_code}'
+    return output
+
+def update_branch_protection():
+    return
+
+if __name__ == '__main__':
+    args_parser = ArgumentParser(prog='github_api', description='Multi-purpose github api script')
+    args_parser.add_argument('--github_api_url', help='Selected github endpoint')
+    args_parser.add_argument('--endpoint', help='Selected github endpoint')
+    args_parser.add_argument('--org', help='Repo owner')
+    args_parser.add_argument('--repo', help='Repo data')
+    args_parser.add_argument('--root', help='Root directory to fetch files', default='../orgs')
+    args = args_parser.parse_args()
+    if args.endpoint == 'manage_collaborators':
+        manage_collaborators(
+            github_api=args.github_api_url,
+            owner=args.org,
+            repo_name=args.repo,
+            repo=read_yaml_file(path=args.root, org=args.org, repo_name=args.repo, endpoint=args.endpoint)
+        )
+    if args.endpoint == 'branch_protection':
+        update_branch_protection()
